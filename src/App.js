db0c57b5be81760482d556999883405eb0a95298
batman <batman@gmail.com>
2023-04-20 00:25:31 +0530

    All components are different files and calling from root function

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import { ReactLogo } from './Components/ReactLogo';
+import { ReactBenifits } from './Components/ReactBenifits';
+import { Button } from './Components/Button';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <ReactLogo/>
+       <ReactBenifits/>
+       <Button/>
     </div>
   );
 }
-
 export default App;
---
+++ src/Components/ReactLogo.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export function ReactLogo(){
+    return(
+        <>
+        <img src="./logo192.png" alt="logo" />
+        </>
+    )
+}
