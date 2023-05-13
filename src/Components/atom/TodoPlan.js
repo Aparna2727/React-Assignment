b526477ad1b0e2d3f436c2e36c0dd8d4d721c27b
batman <batman@gmail.com>
2023-05-13 14:55:31 +0530

    Todo File

--- src/Components/atom/TodoPlan.js
+++ src/Components/atom/TodoPlan.js
@@ -6,7 +6,6 @@ export function TodoList() {
     const [activity, setActivity] = useState("");
     const [listData , setListData] = useState([]);
     const [count , setCount] = useState(0);
-    const [values,setValues] = useState([])
     const [iscompleted,setIscompleted] = useState(false)
     
    
@@ -17,7 +16,7 @@ export function TodoList() {
 
         setListData((listData)=>{
             const updateedList = [...listData, activity];
-            console.log(updateedList);
+            // console.log(updateedList);
             setActivity('');
             setCount(count+1);
             return updateedList;
@@ -26,9 +25,7 @@ export function TodoList() {
     }
 
     function removeActivity(index){
-    const updateedListData = listData.filter((elem , id) => {
-        return index !== id;
-    });
+    const updateedListData = listData.filter((elem , id) => index !== id );
     setListData(updateedListData);
     if(count > 0){
         setCount(count-1)
@@ -36,13 +33,8 @@ export function TodoList() {
     }
 
     function handleComplete(index){
- 
-   const duplicate = [...values];
-    duplicate[index]=false;
-    setValues(duplicate);
-    // console.log(values[index])
 
-    setIscompleted(!iscompleted)
+    setIscompleted((iscompleted)=>!iscompleted)
         if(count > 0){
             setCount(count-1)
         }
@@ -53,20 +45,19 @@ export function TodoList() {
    <div className={styles.header}><h1>Todo List</h1></div>
    <div className={styles.container}>
     <div className={styles.heading}><h2>Pending tasks ({count})</h2></div>
-    {/* <p className='list-heading'>Here is your List</p> */}
 
     <ul className={styles.list}>
     {listData.length !== 0 && listData.map((data, index) =>{
         return(
             <>
-            {/* <p  key ={index}> */}
+          
                 <div  key ={index} className={styles.listData}>
-                <li className={styles.l2}><p className={values[index] ? "" :styles.p2}>{data}</p></li>
+                <li className={styles.l2}><p className={`${iscompleted ? styles.show_line : styles.none_line}`}>{data}</p></li>
 
                    <button className={styles.complete_btn} onClick={()=>handleComplete(index)}>Compelet</button>
                    <button className={styles.X_btn} onClick={()=>removeActivity(index)}>X</button>
                 </div>
-            {/* </p> */}
+           
             </>
         )
     })}
