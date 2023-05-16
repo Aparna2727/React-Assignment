5dbc7e6a25bb94c202648ced491e360104a7ca6c
batman <batman@gmail.com>
2023-05-16 22:32:28 +0530

    This is Delete and Add user name file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useState} from 'react'
+
 
 function App() {
+
+  const [users, setUsers] = useState([]);
+  const [newUser, setNewUser] = useState('');
+
+  function handleDeleteUser(index){
+    const updatedUsers = [...users];
+    updatedUsers.splice(index, 1);
+    setUsers(updatedUsers);
+  };
+
+  function  handleAddUser(){
+    if (newUser.trim() !== '') {
+      setUsers([...users, newUser]);
+      setNewUser('');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h2>Add User's</h2>
+      <ul>
+        {users.map((user, index) => (
+          <li key={index}>
+            {user}{' '}
+            <button className='Delete-btn'  onClick={() => handleDeleteUser(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+      <input type="text" value={newUser} onChange={(event)=>setNewUser(event.target.value)} placeholder="Add new user"/>
+      <button className='Add-btn' onClick={handleAddUser}>Add</button>
     </div>
   );
 }
